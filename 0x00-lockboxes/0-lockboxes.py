aaf4187b18e484e6aa37997901b0d13342b33db5
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-08-04 19:35:42 -0500

    feat: Added lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# canUnlockAll: Checks if a set of boxes (represented as a list of lists) can be opened
+
+def canUnlockAll(boxes):
+    got_keys = [0]
+    total_boxes = list(range(0, len(boxes)))
+    for i in got_keys:
+        box = boxes[i]
+        for key in box:
+            if key not in got_keys:
+                got_keys.append(key)
+        box.clear()
+    for box in total_boxes:
+        if box not in got_keys:
+            return False
+    return True
---
+++ README.md
@@ -0,0 +1,2 @@
+# Holberton School Interviews
+Hi! This repo has many programming interviewing exercises.
