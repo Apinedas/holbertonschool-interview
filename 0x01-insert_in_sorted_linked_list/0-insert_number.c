7566e91ec0a697fd9ea0cb46386b97bc2861b56d
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-08-08 22:37:38 -0500

    fix: Solved edge cases

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -25,12 +25,17 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new_node->n = number;
-	new_node->next = curr->next;
 
 	if (curr != *head)
+	{
 		curr->next = new_node;
+		new_node->next = curr->next;
+	}
 	else
+	{
+		new_node->next = *head;
 		*head = new_node;
+	}
 
 	return (new_node);
 }
