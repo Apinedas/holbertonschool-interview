0b63b43a7da0be7c05f4bf999cea5cde5301fe5c
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-08-18 16:50:31 -0500

    fix: Initialized value

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = NULL;
 
 	new_node = malloc(sizeof(*new_node));
 	if (!new_node)
