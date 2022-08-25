de796c164ae8da483935ba52a4465eb3f29539e1
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-08-24 21:34:22 -0500

    fix: Recursive min ops

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,39 +1,10 @@
 #!/usr/bin/python3
-"""Module to calc min number of operations using prime numbers"""
-
-import math
-
-
-def maxPrimeFactors(n):
-
-    maxPrime = -1
-
-    while n % 2 == 0:
-        maxPrime = 2
-        n >>= 1
-
-    while n % 3 == 0:
-        maxPrime = 3
-        n = n/3
-
-    for i in range(5, int(math.sqrt(n)) + 1, 6):
-        while n % i == 0:
-            maxPrime = i
-            n = n / i
-        while n % (i+2) == 0:
-            maxPrime = i+2
-            n = n / (i+2)
-
-    if n > 4:
-        maxPrime = n
-
-    return int(maxPrime)
+"""Module to calc min number of operations using brute force"""
 
 def minOperations(n):
-
-    maxPrime = maxPrimeFactors(n)
-    if maxPrime == n:
-        return n
-    
-    lastCopyNeeded = n / maxPrime
-    return (int(maxPrime + lastCopyNeeded))
+    i = 2
+    if n != 1:
+        while n % i != 0:
+            i += 1
+        return i + minOperations(int(n/i))
+    return 0
