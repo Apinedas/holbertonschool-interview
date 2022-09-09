f97e875942fd70f62a872873f7f2a294c11d3e84
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-09-09 02:22:40 -0500

    fix: Eficient but poor

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,29 +1,33 @@
 #include "lists.h"
 
 /**
- * is_palindrome - Checks if SLL is palindrome
- * @head: Pointer to SLL head
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
-
 int is_palindrome(listint_t **head)
 {
-	listint_t *curr, *compare;
+    listint_t *current;
+	int i = 0, copied_list[4000];
 
-	if (!head || !*head)
-		return (1);
-
-	curr = *head;
-	compare = curr->next;
-	while (curr->next && curr->next->next)
-	{
-		curr = curr->next;
-		compare = curr->next;
-	}
-
-	if ((*head)->n != compare->n)
-		return (0);
-
-	curr->next = NULL;
-	return (is_palindrome(&(*head)->next));
+    current = *head;
+    while(current)
+    {
+        copied_list[i] = current->n;
+        i += 1;
+        current = current->next;
+    }
+    current = *head;
+    i -= 1;
+    while (current)
+    {
+        if (current->n == copied_list[i])
+        {
+            i -= 1;
+            current = current->next;
+        }
+        else
+            return (0);
+    }
+    return (1);
 }
