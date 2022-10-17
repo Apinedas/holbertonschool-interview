1ccd7f6a513b6f5d02c5b515c6310b242861ffcf
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-16 22:26:36 -0500

    feat: Added UTF8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''UTF8 Validator'''
+
+
+def validUTF8(data):
+    '''Validates UTF-8 Through binary conversion'''
+    i = 0
+    while i < len(data):
+        binary_byte = bin(data[i]).split('b')[1]
+        while len(binary_byte) < 8:
+            binary_byte = '0' + binary_byte
+        else:
+            binary_byte = binary_byte[-8:]
+        total_ones_in_a_row = 0
+        if binary_byte[0] == '0':
+            i += 1
+            continue
+        
+        for j in range(0, 4):
+            if binary_byte[j] == '1':
+                total_ones_in_a_row += 1
+            else:
+                break
+        
+        if total_ones_in_a_row < 2:
+            return False
+        
+        for j in range(i + 1, i + total_ones_in_a_row):
+            actual_binary_byte = bin(data[j])
+            if actual_binary_byte[0:1] != '10':
+                return False
+            i += 1
+        
+        i += 1
+
+    return True
