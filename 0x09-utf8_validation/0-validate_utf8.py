1ce6447c5676ffde98165bb1b3be52df1484ad0d
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-10-16 22:32:06 -0500

    fix: Second try

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -15,22 +15,22 @@ def validUTF8(data):
         if binary_byte[0] == '0':
             i += 1
             continue
-        
+
         for j in range(0, 4):
             if binary_byte[j] == '1':
                 total_ones_in_a_row += 1
             else:
                 break
-        
+
         if total_ones_in_a_row < 2:
             return False
-        
+
         for j in range(i + 1, i + total_ones_in_a_row):
-            actual_binary_byte = bin(data[j])
-            if actual_binary_byte[0:1] != '10':
+            actual_binary_byte = bin(data[j]).split('b')[1]
+            if actual_binary_byte[0:2] != '10':
                 return False
             i += 1
-        
+
         i += 1
 
     return True
