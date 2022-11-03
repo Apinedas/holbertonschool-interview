848ba92f50f675f0fccb0f946011bdfa691491bb
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-11-02 20:43:12 -0500

    fix: New N queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -17,56 +17,48 @@ if (N < 4):
     print("N must be at least 4")
     exit(1)
 
+def solveNQueens(n):
+    col = set()
+    posDiag = set()
+    negDiag = set()
 
-def nqueens(actual_row, actual_col, N):
+    res = []
+    board = [["."] * n for _ in range(n)]
 
-    attacked_pos_diags = []
-    attacked_neg_diags = []
-    attacked_rows = []
-    attacked_cols = []
-    on_board_queens = []
+    def backtrack(r):
+        if r == n:
+            copy = ["".join(row) for row in board]
+            res.append(copy)
+            return
+        
+        for c in range(n):
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                continue
+        
+            col.add(c)
+            posDiag.add(r + c)
+            negDiag.add(r - c)
+            board[r][c] = "Q"
 
-    while actual_row < N:
-        actual_pos_diag = actual_row - actual_col
-        actual_neg_diag = actual_row + actual_col
-        if actual_col < N and actual_row not in attacked_rows\
-           and actual_col not in attacked_cols\
-           and actual_pos_diag not in attacked_pos_diags\
-           and actual_neg_diag not in attacked_neg_diags:
-            attacked_rows.append(actual_row)
-            attacked_cols.append(actual_col)
-            attacked_pos_diags.append(actual_pos_diag)
-            attacked_neg_diags.append(actual_neg_diag)
-            on_board_queens.append([actual_row, actual_col])
-            actual_row += 1
-            actual_col = 0
-        else:
-            actual_col += 1
-            if actual_row == 0 and actual_col >= N:
-                return None
-            if actual_col >= N:
-                actual_row, actual_col = on_board_queens.pop(-1)
-                actual_pos_diag = actual_row - actual_col
-                actual_neg_diag = actual_row + actual_col
-                attacked_rows.remove(actual_row)
-                attacked_cols.remove(actual_col)
-                attacked_pos_diags.remove(actual_pos_diag)
-                attacked_neg_diags.remove(actual_neg_diag)
-                actual_col += 1
+            backtrack(r + 1)
 
-    return (on_board_queens)
+            col.remove(c)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
+            board[r][c] = "."
+    
+    backtrack(0)
+    return res
 
 
-actual_row = 0
-actual_col = 0
-solutions = []
+results = []
+boards = solveNQueens(N)
+for board in range(len(boards)):
+    results.append([])
+    for row in range(len(boards[board])):
+        for col in range(len(boards[board][row])):
+            if boards[board][row][col] == 'Q':
+                results[board].append([row, col])
 
-while actual_col < N:
-    solution = nqueens(actual_row, actual_col, N)
-    if solution:
-        solutions.append(solution)
-        actual_row, actual_col = solution[0]
-    actual_col += 1
-
-for solution in solutions:
-    print(solution)
+for res in results:
+    print(res)
