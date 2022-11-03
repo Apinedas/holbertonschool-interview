d5ac0980e29bf954a5f61cb21fc487edd9e39335
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-11-02 20:45:30 -0500

    fix: Style

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -17,6 +17,7 @@ if (N < 4):
     print("N must be at least 4")
     exit(1)
 
+
 def solveNQueens(n):
     col = set()
     posDiag = set()
@@ -30,11 +31,11 @@ def solveNQueens(n):
             copy = ["".join(row) for row in board]
             res.append(copy)
             return
-        
+
         for c in range(n):
             if c in col or (r + c) in posDiag or (r - c) in negDiag:
                 continue
-        
+
             col.add(c)
             posDiag.add(r + c)
             negDiag.add(r - c)
@@ -46,7 +47,7 @@ def solveNQueens(n):
             posDiag.remove(r + c)
             negDiag.remove(r - c)
             board[r][c] = "."
-    
+
     backtrack(0)
     return res
 
