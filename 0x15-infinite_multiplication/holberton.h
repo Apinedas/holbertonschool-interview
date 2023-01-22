94a63401821317d0889b73676dbec8991ccf7524
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-01-22 18:11:34 -0500

    feat: Added infinite mul

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int _putchar(char c);
+int _isdigit(char *n);
+int _strlen(char *s);
+char *infinite_multiply(char *s1, char *s2);
+
+#endif /* HOLBERTON_H */
